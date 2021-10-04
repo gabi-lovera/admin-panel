f6e2aabdb543cfe8d2c340f12fe4411ce2a16d48
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-10-04 14:53:28 -0300

    charts from react-chartjs-2 library

---
+++ src/charts/BarChart.js
@@ -0,0 +1,43 @@
+import { Bar } from "react-chartjs-2";
+
+const data = {
+  labels: ["1", "2", "3", "4", "5", "6"],
+  datasets: [
+    {
+      label: "Usuarios",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: "rgb(255, 99, 132)",
+    },
+    {
+      label: "Ventas",
+      data: [2, 3, 20, 5, 1, 4],
+      backgroundColor: "rgb(54, 162, 235)",
+    },
+    {
+      label: "Nuevos",
+      data: [3, 10, 13, 15, 22, 30],
+      backgroundColor: "rgb(75, 192, 192)",
+    },
+  ],
+};
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+const BarChart = () => (
+  <>
+    <div className="header">
+      <h1 className="title">Gráfico de Barras</h1>
+    </div>
+    <Bar data={data} options={options} />
+  </>
+);
+
+export default BarChart;
